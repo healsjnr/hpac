f461b4b4f43f5b002d26643728ef5f494878135a
healsjnr <healsjnr@gmail.com>
2014-01-13 21:49:26 +1000

    changing rendering to use single function

--- lib/hpac_app.rb
+++ lib/hpac_app.rb
@@ -3,13 +3,28 @@ require 'sinatra/base'
 require 'haml'
 
 class HpacApp < Sinatra::Base
-  attr_accessor :about_text, :where_text, :when_text, :who_text 
+
+  configure do 
+  end
+
+  helpers do
+    def render_text(text)
+      case text
+      when :about_text
+       'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
+      when :where_text
+        'Grand Hirafu, Niseko, Hokaido Japan.'
+      when :when_text
+        'Early January 2015.'
+      when :who_text
+        'A list of speakers will be announced shortly.'
+      else 
+        ''
+      end
+    end
+  end
   
   get '/' do
-    @about_text = 'Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.'
-    @where_text = 'Grand Hirafu, Niseko, Hokaido Japan.'
-    @when_text = 'Early January 2015.'
-    @who_text = 'A list of speakers will be announced shortly.'
     haml :app, { :layout => :layout }
   end
 
