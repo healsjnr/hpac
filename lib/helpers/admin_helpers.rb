8dd07f73e9580f28486e70562112863f7893171a
healsjnr <healsjnr@gmail.com>
2014-01-24 08:07:12 +1000

    admin page now has drop down for pages with multiple content

--- lib/helpers/admin_helpers.rb
+++ lib/helpers/admin_helpers.rb
@@ -1,3 +1,18 @@
 module AdminHelpers
+  def content_name content
+    return content.title if content.page.nil?
+    "#{content.page.capitalize}: #{content.title}"
+  end
+
+  def site_content_titles
+    contents = Content.order(:page => :asc, :title => :asc)
+    titles = {}
+    # Change to map
+    contents.each do |cont|
+      titles[cont.page] ||= {}
+      titles[cont.page][cont.content_id] = cont.title 
+    end
+    titles
+  end
 end
 
